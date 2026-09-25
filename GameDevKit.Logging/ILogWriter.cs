6128c64e4edbe334a47d4471ebc3349355b1ba68
3cxc <81449257+3cxc@users.noreply.github.com>
2026-09-25 17:48:35 +0800

    feat(log): 日志框架
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

---
+++ GameDevKit.Logging/ILogWriter.cs
@@ -0,0 +1,29 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+namespace GameDevKit.Logging.Abstractions
+{
+    /// <summary>
+    /// 日志写入器抽象，由引用端自行实现
+    /// </summary>
+    public interface ILogWriter
+    {
+        /// <summary>
+        /// 写入一条调试级别日志
+        /// </summary>
+        void WriteDebug(string message);
+
+        /// <summary>
+        /// 写入一条信息级别日志
+        /// </summary>
+        void WriteInformation(string message);
+
+        /// <summary>
+        /// 写入一条警告级别日志
+        /// </summary>
+        void WriteWarning(string message);
+
+        /// <summary>
+        /// 写入一条错误级别日志
+        /// </summary>
+        void WriteError(string message, Exception? ex = null);
+    }
+}
---
+++ GameDevKit.Logging/Log.cs
@@ -0,0 +1,28 @@
+﻿// SPDX-License-Identifier: MPL-2.0
+namespace GameDevKit.Logging.Abstractions
+{
+    public static class Log
+    {
+        public static ILogWriter? Writer { get; set; }
+
+        public static void Debug(string message)
+        {
+            Writer?.WriteDebug(message);
+        }
+
+        public static void Print(string message)
+        {
+            Writer?.WriteInformation(message);
+        }
+
+        public static void PrintWarning(string message)
+        {
+            Writer?.WriteWarning(message);
+        }
+
+        public static void PrintErr(string message, Exception? ex = null)
+        {
+            Writer?.WriteError(message, ex);
+        }
+    }
+}
