36ae5839187e44549e8abb4bda29c0ce2c30fdbd
3cxc <81449257+3cxc@users.noreply.github.com>
2026-09-25 19:53:38 +0800

    chore(license):  将许可证修改为 EPL-2.0
    BREAKING CHANGE: 许可证由 MPL-2.0 OR BSD-3-Clause 变更为 EPL-2.0
    
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- GameDevKit.Core/MainThreadScheduler.cs
+++ GameDevKit.Core/MainThreadScheduler.cs
@@ -1,4 +1,11 @@
-﻿// SPDX-License-Identifier: MPL-2.0
+﻿// SPDX-FileCopyrightText: 2026 穹空网络(GoWelkin Network)
+//
+// SPDX-License-Identifier: EPL-2.0
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0/
+
 using System.Collections.Concurrent;
 
 namespace GameDevKit.Core
